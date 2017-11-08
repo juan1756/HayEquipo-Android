b076d8eb900db6c836efb73d308e57156418bc57
German <gerjehova@gmail.com>
2017-11-08 14:26:12 -0300

    crear partido, invitar amigos (amigos harcodeados), modificar partido, buscar jugadores y asociarlos a partidos. ver partidos.

---
+++ app/src/main/java/edu/uade/sip2/hayequipo_android/data/Menus.java
@@ -0,0 +1,30 @@
+package edu.uade.sip2.hayequipo_android.data;
+
+/**
+ * Created by Usuario on 08/11/2017.
+ */
+
+public class Menus {
+
+    private Menus() {
+    }
+
+    public static final String PARTIDOS[] = {
+            "PARTIDO 1","PARTIDO 2","PARTIDO 3","PARTIDO 4","PARTIDO 5","PARTIDO 6","PARTIDO 7","PARTIDO 8","PARTIDO 9","PARTIDO 10", "PARTIDO X","ETC..."
+
+    };
+
+
+    public static final String AMIGOS[] = {
+            "AMIGO 1","AMIGO 2","AMIGO 3","AMIGO 4","AMIGO 5","AMIGO 6","AMIGO 7","ETC..."
+
+    };
+
+
+    public static final String SOLICITUDES[] = {
+            "SOLICIUD 1","SOLICITUD 2","ETC..."
+
+    };
+
+
+}
